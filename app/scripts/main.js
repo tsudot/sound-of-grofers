d775ed12e61a8178c3f6870255515fb5e1ae2bdf
Kunal Kerkar <me@tsudot.com>
2017-02-09 13:13:28 +0530

    Color change for different events

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,37 +4,42 @@ function sleep(ms) {
 
 
 $(document).ready(function() {
-    var ws = new WebSocket("ws://172.31.80.41:8086/");
+    var EVENT_POPUP_WINDOW = 20;
+    var WS_HOST = "172.31.80.41"
+    var WS_PORT = 8086
 
-	var order_bombs = [];
+    var ws = new WebSocket("ws://" + WS_HOST + ":" + WS_PORT + "/");
 
-	var EVENT_POPUP_WINDOW = 20;
+    var receive_order_bombs = [];
+    var other_bombs = [];
+    var receive_order_ui_pops = [];
+    var other_ui_pops = [];
 
-	// Write your code in the same way as for native WebSocket:
-	ws.onopen = function() {
-		ws.send("Hello");  // Sends a message.
-	};
+    // Write your code in the same way as for native WebSocket:
+    ws.onopen = function() {
+        ws.send("Hello");  // Sends a message.
+    };
 
-	ws.onmessage = function(e) {
-		// Receives a message.
-		var data = JSON.parse(e.data)
+    ws.onmessage = function(e) {
+        // Receives a message.
+        var data = JSON.parse(e.data)
 
-		if (data['event'] == "receive_order") {
-			order_bombs.push(data['data'][0])
-			console.log('bombs')
-			console.log(bombs);
-			console.log('order_bombs');
-			console.log(order_bombs.slice(-EVENT_POPUP_WINDOW));
-			world_map.bubbles(order_bombs.slice(-EVENT_POPUP_WINDOW));
-		}
+        if (data['event'] == "receive_order") {
+            receive_order_bombs.push(data['data'][0])
+            receive_order_ui_pops = receive_order_bombs.slice(-EVENT_POPUP_WINDOW);
+        }
+        else {
+            other_bombs.push(data['data'][0])
+            other_ui_pops = other_bombs.slice(-EVENT_POPUP_WINDOW);
+        }
 
-	};
+        world_map.bubbles(receive_order_ui_pops.concat(other_ui_pops));
 
-	ws.onclose = function() {
-		console.log("closed");
-	};
+    };
 
-
+    ws.onclose = function() {
+        console.log("closed");
+    };
 
     var world_map = new Datamap({
         element: document.getElementById('map'),
@@ -48,31 +53,35 @@ $(document).ready(function() {
             defaultFill: '#ccc',
             'add': '#306596',
             'subtract': '#cc4731',
-			'orders': '#3296DB'
+            'receive_order': '#3296DB',
+            'other': '#F26E21'
         },
-		data: {
-			'receive_order': {fillKey: 'receive_order'}
-		},
+        data: {
+            'receive_order': {fillKey: 'receive_order'},
+            'other': {fillKey: 'other'}
+        },
+
         response: true,
         setProjection: function(element) {
             var projection = d3.geo.equirectangular()
                 .center([80, 25])
-                .scale(700)
+                .scale(800)
                 .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
             var path = d3.geo.path()
                 .projection(projection);
 
             return {path: path, projection: projection};
         },
-		bubblesConfig: {
-			popupTemplate: function (geo, data) {
-				return ['<div class="hoverinfo">' +  data.name,
-					'<br/>User: ' + data.user + '',
-					'<br/>Country: ' +  data.country + '',
-					'</div>'].join('');
-			}
-		},
-
+        bubblesConfig: {
+            popupTemplate: function (geo, data) {
+                return ['<div class="hoverinfo">' +  data.name,
+                    '<br/>User: ' + data.user + '',
+                    '<br/>Country: ' +  data.country + '',
+                    '</div>'].join('');
+            },
+            borderWidth: 0,
+            borderOpacity: 0
+        },
     });
 
     var bombs = [{
@@ -97,18 +106,4 @@ $(document).ready(function() {
     ];
 
     world_map.bubbles([]);
-/**
-    var socket = io('http://172.31.80.41:8086/');
-    socket.on("connect", function () {
-        console.log("Connected!");
-    });
-    socket.on('receive_order', function(message){
-        console.log(message);
-        var data = JSON.parse(message);
-
-        world_map.bubbles(data);
-    })
-**/
 });
-
-
